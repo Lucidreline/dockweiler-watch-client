89c927d76136abb3b1dcd9580733d5275c75bd72
Lucidreline <mcastaneda82@toromail.csudh.edu>
2021-10-16 22:30:40 -0700

    Started the basic styling of the product card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
 import './App.scss'
 import ProductCard from './components/product-card/product-card.component'
 
-// eslint-disable-next-line react/react-in-jsx-scope
-const App = () => <ProductCard />
+const App = () => (
+  <div id="margin-container">
+    <ProductCard />
+  </div>
+)
 
 export default App
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -4,6 +4,10 @@ import './product-card.styles.scss'
 const ProductCard = () => (
   <div className="product-card">
     <div className="image" />
+    <div className="price-details">
+      <span className="sale-status">Full Price</span>
+      <span className="price">$50</span>
+    </div>
   </div>
 )
 
